cd5bb378119ef075683d617710f7a05335dec866
Nina Argillander <ninar253@student.liu.se>
2018-11-26 16:35:40 +0100

    Något funkar inte som det ska, fixar på onsdag

--- src/SetTimerIntervall.js
+++ src/SetTimerIntervall.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react'
+let pauseTime = [0,15];
 
 class SetTimerIntervall extends Component {
+	
 
 	constructor(props) {
 		super(props)
-
+		
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
-
+	
 
 	handleSubmit(event) {
 
 		event.preventDefault();
-		this.props.setTimeState(event.target.elements.inputTime.value);
-		
-		console.log("in handleSubmit" + event.target.elements.inputTime.value);
+		this.props.setTimeState(event.target.elements.inputTimeH.value, event.target.elements.inputTimeMin.value);
+
+		this.props.setPauseState(event.target.elements.inputTimePauseH.value, event.target.elements.inputTimePauseMin.value);
+	
 
 	}
 
@@ -23,9 +26,13 @@ class SetTimerIntervall extends Component {
 			<div>
 				<form onSubmit={this.handleSubmit}>
 					<label>
-						Plugg: 
-						<input type="number" name="inputTime" defaultValue={this.props.defaultTime} />
-						
+						Pluggtid Timmar:
+						<input type="number" name="inputTimeH" defaultValueH={this.props.defaultTime} />
+						Minuter: <input type="number" name="inputTimeMin" defaultValueMin={this.props.defaultTime} />
+				
+						Paustid Timmar:
+						<input type="number" name="inputTimePauseH" defaultValuePauseH={this.props.defaultTime} />
+						Minuter: <input type="number" name="inputTimePauseMin" defaultValuePauseMin={this.props.defaultTime} />
 					</label>
 						<input type="submit" value="Submit"/>
 				</form>
