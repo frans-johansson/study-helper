230e587bfa88c3d1da7ea8047ab59c56e46a0e2c
Nina Argillander <ninar253@student.liu.se>
2018-12-05 11:26:35 +0100

    Lägger till funktion för att formattera till timmar-minuter-sträng

--- src/Utils.js
+++ src/Utils.js
@@ -6,5 +6,17 @@ export function formatTimeToUser(seconds) {
 	let date = new Date(null)
 	date.setSeconds(seconds)
 
+	console.log(seconds)
+
 	return date.toISOString().substr(11, 8)
+}
+
+export function toHoursMinutes(h) {
+	let seconds = h*3600
+	let timeString = formatTimeToUser(seconds)
+
+	let hours = timeString.substring(0, 2)
+	let minutes = timeString.substring(3, 5)
+
+	return(`${hours}h ${minutes}min`)
 }
